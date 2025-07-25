25c860871c0bfd5d1ff142ea0ae89f9ec2927532
pmsoares <pmsoares@gmail.com>
2025-07-25 13:30:15 +0100

    Sessão de 25/07/2025

--- YumBlazor/Repository/IRepository/ICategoryRepository.cs
+++ YumBlazor/Repository/IRepository/ICategoryRepository.cs
@@ -4,14 +4,14 @@ namespace YumBlazor.Repository.IRepository
 {
     public interface ICategoryRepository
     {
-        public Category Create(Category obj);
+        public Task<Category> CreateAsync(Category obj);
 
-        public Category Update(Category obj);
+        public Task<Category> UpdateAsync(Category obj);
 
-        public bool Delete(int id);
+        public Task<bool> DeleteAsync(int id);
 
-        public Category Get(int id);
+        public Task<Category> GetAsync(int id);
 
-        public IEnumerable<Category> GetAll();
+        public Task<IEnumerable<Category>> GetAllAsync();
     }
 }
