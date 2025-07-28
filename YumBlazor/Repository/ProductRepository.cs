15cf091b2fe484aada03768a03ce4a06e62f6ef3
pmsoares <pmsoares@gmail.com>
2025-07-28 13:05:34 +0100

    Sessão de 28/07/2025

---
+++ YumBlazor/Repository/ProductRepository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YumBlazor.Data;
+using YumBlazor.Repository.IRepository;
+
+namespace YumBlazor.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ProductRepository(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
+        {
+            _db = db;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<Product> CreateAsync(Product obj)
+        {
+            await _db.Products.AddAsync(obj);
+            await _db.SaveChangesAsync();
+            return obj;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var obj = await _db.Products.FirstOrDefaultAsync(u => u.Id == id);
+            var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, obj.ImageUrl!.TrimStart('/'));
+
+            if(File.Exists(imagePath))
+                File.Delete(imagePath);
+
+            if (obj != null)
+            {
+                _db.Products.Remove(obj);
+                return (await _db.SaveChangesAsync()) > 0;
+            }
+            return false;
+        }
+
+        public async Task<Product> GetAsync(int id)
+        {
+            var obj = await _db.Products.FirstOrDefaultAsync(u => u.Id == id);
+            if (obj == null)
+                return new Product();
+            
+            return obj;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _db.Products.Include(u=>u.Category).ToListAsync();
+        }
+
+        public async Task<Product> UpdateAsync(Product obj)
+        {
+            var objFromDb = await _db.Products.FirstOrDefaultAsync(u => u.Id == obj.Id);
+
+            if (objFromDb != null)
+            {
+                objFromDb.Name = obj.Name;
+                objFromDb.Description = obj.Description;
+                objFromDb.Price = obj.Price;
+                objFromDb.SpecialTag = obj.SpecialTag;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.ImageUrl = obj.ImageUrl;
+
+                _db.Products.Update(objFromDb);
+                await _db.SaveChangesAsync();
+                return objFromDb;
+            }
+
+            return obj;
+        }
+    }
+}
